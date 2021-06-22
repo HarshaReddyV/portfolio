bb14a67e8b20d43a09071c0a0ee773ce1f2ea6ff
HarshaReddyV <HarshaReddyV@users.noreply.github.com>
2021-06-22 14:24:03 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -19,7 +19,7 @@ int main(void)
    {
       y=i % 10;
       i=i/10;
-    
+
 
       if(x%2==0)
       {
@@ -58,7 +58,7 @@ int main(void)
    }
    {
     a = sum1+sum2;
-      printf("sum1%i\n sum2%i\n",sum1,sum2);
+
 
         if(a%10==0)
         {
