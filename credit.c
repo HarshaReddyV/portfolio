208c5b8361f7acda0cc94a1563710c7d046a6a6e
HarshaReddyV <HarshaReddyV@users.noreply.github.com>
2021-06-24 10:41:45 +0000

    automated commit by submit50

--- credit.c
+++ credit.c
@@ -3,74 +3,69 @@
 
 int main(void)
 {
-   long i,b;
-   int y, x, z, l, a,d,c, sum1, sum2;
-   sum2=0;
-   sum1=0;
+    long i, b;
+    int y, x, z, l, a,d,c, sum1, sum2;
+    sum2 = 0;
+    sum1 = 0;
 
-   do
+    do
+    {
+      i = get_long("Enter Card Number:");
+    }
+    while (i < 0);
+    b = i;
+
+    for ( x = 0; i != 0; x++)
    {
-      i = get_long( "Enter Card Number:" );
-   }
-   while (i < 0);
-   b=i;
-
-   for(x=0;i!=0;x++)
-   {
-      y=i % 10;
-      i=i/10;
+        y = i % 10;
+        i = i/10;
 
 
-      if(x%2==0)
-      {
-        sum1 = sum1 + y;
-
-
-
-      }
-      else
-      {
-
-        y = y * 2;
-        if(y >= 10)
+        if (x % 2 == 0)
         {
-           for(z = 0; y > 0; z++)
-           {
-             l = y % 10;
-             y = y / 10;
-             sum2 = sum2 + l;
-
-
-           }
-           {
-
-           }
-        }
-        else
-        {
-           sum2 = sum2 + y;
-
-
+            sum1 = sum1 + y;
 
 
 
         }
+        else
+        {
 
-      }
+            y = y * 2;
+            if(y >= 10)
+            {
+               for(z = 0; y > 0; z++)
+               {
+                   l = y % 10;
+                   y = y / 10;
+                   sum2 = sum2 + l;
+               }
+               {
+
+
+               }
+           }
+
+           else
+           {
+               sum2 = sum2 + y;
+           }
+
+        }
 
    }
    {
-    a = sum1+sum2;
+    a = sum1 + sum2;
 
 
 
 
-        if(a%10==0)
+        if ( a % 10 == 0)
         {
 
-           for(d=1;b>99;d++)
+           for( d = 1; b > 99; d++)
            {
-             c = b%10;
+             c = b % 10;
              b =  b / 10;
 
 
@@ -79,9 +74,9 @@ int main(void)
            {
 
 
-              if(b==34 || b==37)
+              if(b == 34 || b == 37)
               {
-                 if(d==14)
+                 if(d == 14)
                  {
                      printf("AMEX\n");
                  }
@@ -91,9 +86,9 @@ int main(void)
                  }
 
               }
-              else if(b>=51 && b<=55)
+              else if(b >= 51 && b <= 55)
               {
-                  if(d==15)
+                  if(d == 15)
                   {
                      printf("MASTERCARD\n");
                   }
@@ -103,9 +98,9 @@ int main(void)
                   }
 
               }
-              else if(b>=40 && b<=49)
+              else if(b >= 40 && b <= 49)
               {
-                if(d==12 || d==15)
+                if(d == 12 || d == 15)
                 {
                     printf("VISA\n");
                 }
