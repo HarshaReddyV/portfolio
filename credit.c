177ebbe0c93f29631cebcea2c1b86b3705f58da2
HarshaReddyV <HarshaReddyV@users.noreply.github.com>
2021-06-24 11:02:35 +0000

    automated commit by submit50

--- credit.c
+++ credit.c
@@ -4,21 +4,21 @@
 int main(void)
 {
     long i, b;
-    int y, x, z, l, a,d,c, sum1, sum2;
+    int y, x, z, l, a, d, c, sum1, sum2;
     sum2 = 0;
     sum1 = 0;
 
     do
     {
-      i = get_long("Enter Card Number:");
+        i = get_long("Enter Card Number:");
     }
     while (i < 0);
     b = i;
 
-    for ( x = 0; i != 0; x++)
-   {
+    for (x = 0; i != 0; x++)
+    {
         y = i % 10;
-        i = i/10;
+        i = i / 10;
 
 
         if (x % 2 == 0)
@@ -32,94 +32,94 @@ int main(void)
         {
 
             y = y * 2;
-            if(y >= 10)
+            if (y >= 10)
             {
-               for(z = 0; y > 0; z++)
-               {
-                   l = y % 10;
-                   y = y / 10;
-                   sum2 = sum2 + l;
-               }
-               {
+                for (z = 0; y > 0; z++)
+                {
+                    l = y % 10;
+                    y = y / 10;
+                    sum2 = sum2 + l;
+                }
+            }
 
 
-               }
-           }
+            else
+            {
+                sum2 = sum2 + y;
+            }
+        }
 
-           else
-           {
-               sum2 = sum2 + y;
-           }
+    }
+    {
+
+        a = sum1 + sum2;
+
+
+
+
+        if (a % 10 == 0)
+        {
+
+            for (d = 1; b > 99; d++)
+            {
+                c = b % 10;
+                b =  b / 10;
+            }
+
+            {
+
+                if (b == 34 || b == 37)
+                {
+
+                    if (d == 14)
+                    {
+                        printf("AMEX\n");
+                    }
+                    else
+                    {
+                        printf("INVALID\n");
+                    }
+                }
+
+
+                else if (b >= 51 && b <= 55)
+                {
+                    if (d == 15)
+                    {
+                        printf("MASTERCARD\n");
+                    }
+                    else
+                    {
+                        printf("INVALID\n");
+                    }
+                }
+
+
+                else if (b >= 40 && b <= 49)
+                {
+                    if (d == 12 || d == 15)
+                    {
+                        printf("VISA\n");
+                    }
+                    else
+                    {
+                        printf("INVALID\n");
+                    }
+
+                }
+
+                else
+                {
+                    printf("INVALID\n");
+                }
+            }
 
         }
 
-   }
-   {
-    a = sum1 + sum2;
-
-
-
-
-        if ( a % 10 == 0)
-        {
-
-           for( d = 1; b > 99; d++)
-           {
-             c = b % 10;
-             b =  b / 10;
-
-
-
-           }
-           {
-
-
-              if(b == 34 || b == 37)
-              {
-                 if(d == 14)
-                 {
-                     printf("AMEX\n");
-                 }
-                 else
-                 {
-                    printf("INVALID\n");
-                 }
-
-              }
-              else if(b >= 51 && b <= 55)
-              {
-                  if(d == 15)
-                  {
-                     printf("MASTERCARD\n");
-                  }
-                  else
-                  {
-                     printf("INVALID\n");
-                  }
-
-              }
-              else if(b >= 40 && b <= 49)
-              {
-                if(d == 12 || d == 15)
-                {
-                    printf("VISA\n");
-                }
-                else
-                {
-                   printf("INVALID\n");
-                }
-
-              }
-              else
-              {
-                 printf("INVALID\n");
-              }
-            }
-         }
         else
         {
 
-          printf("INVALID\n");
+            printf("INVALID\n");
         }
-   }
+    }
 }
