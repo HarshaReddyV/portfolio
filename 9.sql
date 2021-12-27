3d7196766e5e72e4ee59400a28343f933bc50a5c
HarshaReddyV <HarshaReddyV@users.noreply.github.com>
2021-12-27 14:20:39 +0000

    automated commit by check50 [check50=True]

--- 9.sql
+++ 9.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT name FROM people,movies WHERE year = 2004 AND people.id = movies.id ORDER BY (birth);
+SELECT DISTINCT name FROM people,movies,stars WHERE year = 2004 AND people.id = stars.person_id AND stars.person_id = movies.id ORDER BY (birth);
