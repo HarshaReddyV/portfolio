3e760e3782a1756635e3fa3aaa77bc777000e403
HarshaReddyV <HarshaReddyV@users.noreply.github.com>
2021-12-31 15:23:09 +0000

    automated commit by submit50

--- application.py
+++ application.py
@@ -6,6 +6,8 @@ from flask import Flask,  redirect, render_template, request
 
 app = Flask(__name__)
 
+db = SQL("sqlite:///reports.db")
+
 
 @app.route("/", methods=["GET", "POST"])
 
@@ -17,7 +19,9 @@ def index():
         s = request.form.get("sole")
         b = request.form.get("business")
 
+        db.execute("INSERT INTO records(personal_income,sole_income,business_income) VALUES(?,?,?)", p,s,b)
 
+        return render_template("index.html")
 
 
     return render_template("index.html")
