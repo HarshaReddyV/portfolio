fc584059c0e5ab6f26bc17af213f8bff68954a2d
Harsha Vardhan Reddy Vanga <71230485+HarshaReddyV@users.noreply.github.com>
2023-04-11 17:46:10 +0100

    Delete test_um.py

--- test_um.py
+++
@@ -1,7 +0,0 @@
-from um import count
-
-def test_count():
-    assert count('um i am um harsha') == 2
-    assert count('um i am um harsha and i have umbrella') == 2
-    assert count('UM UMbrella um') == 2
-    assert count('um, hummer is a um nice car') == 2
