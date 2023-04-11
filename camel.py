83703d017f88ce64741f87984d984a9745aa9c88
Harsha Vardhan Reddy Vanga <71230485+HarshaReddyV@users.noreply.github.com>
2023-04-11 17:41:37 +0100

    Delete camel.py

--- camel.py
+++
@@ -1,8 +0,0 @@
-camel = input('camelCase:')
-
-print('snake_case:',end='')
-for c in camel:
-    if c.islower():
-        print(c,end="")
-    elif c.isupper():
-        print('_'+ c.lower(),end="")
