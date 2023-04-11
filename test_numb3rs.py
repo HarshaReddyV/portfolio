af28c84a64fa89891630c4b5101801a500d5c7f7
Harsha Vardhan Reddy Vanga <71230485+HarshaReddyV@users.noreply.github.com>
2023-04-11 17:45:38 +0100

    Delete test_numb3rs.py

--- test_numb3rs.py
+++
@@ -1,8 +0,0 @@
-from numb3rs import validate
-
-def test_validate():
-    assert validate('cat') == False
-    assert validate('127.127.1.1') == True
-    assert validate('1.1.1.1') == True
-    assert validate('1') == False
-    assert validate('543.1.1.1') == False
