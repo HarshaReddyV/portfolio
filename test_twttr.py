20dbbf7994f9b882c2950b0cc428a01d96e069c8
Harsha Vardhan Reddy Vanga <71230485+HarshaReddyV@users.noreply.github.com>
2023-04-11 17:45:59 +0100

    Delete test_twttr.py

--- test_twttr.py
+++
@@ -1,8 +0,0 @@
-from twttr import shorten
-
-def test_twttr():
-    assert shorten('twitter') == 'twttr'
-    assert shorten("TWITTER") == "TWTTR"
-    assert shorten("TWI'TTER'") == "TW'TTR'"
-    assert shorten('TWITTER55,15') == 'TWTTR55,15'
-
