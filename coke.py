cba86071442198a5554d9a48e45a700bc75cb5e3
Harsha Vardhan Reddy Vanga <71230485+HarshaReddyV@users.noreply.github.com>
2023-04-11 17:41:44 +0100

    Delete coke.py

--- coke.py
+++
@@ -1,8 +0,0 @@
-due=50
-while due > 0:
-    print(f'Amount Due: {due}')
-    coin = int(input('Insert Coin:'))
-    if coin in [25,10,5]:
-        due = due - coin
-
-print(f'Change Owed: {abs(due)}')
